6fff9c2781f1cb1b3f0dc491d70db4f7514b20e4
Nathan RABET <nathan.rabet@epita.fr>
2022-02-12 14:26:23 +0100

    beware_overflow 2

--- beware_overflow/beware_overflow.c
+++ beware_overflow/beware_overflow.c
@@ -9,9 +9,9 @@ void *beware_overflow(void *ptr, size_t nmemb, size_t size)
     if (__builtin_umull_overflow(nmemb, size, &total_elements))
         return NULL;
 
-    unsigned long lptr = ptr - NULL;
+    unsigned long lptr = (unsigned long)ptr;
     if (__builtin_uaddl_overflow(lptr, total_elements, &lptr))
         return NULL;
 
-    return ptr + total_elements;
+    return (void *)lptr;
 }
